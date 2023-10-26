52d617dc891e423f8d0849a1dd2cd28ec557f040
Chesnokov Arkady <arkady-chesnokof@yandex.ru>
2023-10-26 19:14:07 +0300

    add @Column to fields of Coach class & add test sql inserts to coaches table

---
+++ api/vtb-stat/src/main/java/com/d23alex/vtbstat/controllers/CoachController.java
@@ -0,0 +1,7 @@
+package com.d23alex.vtbstat.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CoachController {
+}
--- api/vtb-stat/src/main/java/com/d23alex/vtbstat/entities/Coach.java
+++ api/vtb-stat/src/main/java/com/d23alex/vtbstat/entities/Coach.java
@@ -16,8 +16,12 @@ public class Coach {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+    @Column(name = "first_name")
     String firstName;
+    @Column(name = "last_name")
     String lastName;
+    @Column(name = "date_of_birth")
     Date dateOfBirth;
+    @Column(name = "description")
     String description;
 }
