e98b634c0b8fadad204e3f7d2ea3dab04ea373d2
Chesnokov Arkady <arkady-chesnokof@yandex.ru>
2023-11-01 14:07:59 +0300

    delene unnecessary imports

--- api/vtb-stat/src/main/java/com/d23alex/vtbstat/db/repository/ArenaRepository.java
+++ api/vtb-stat/src/main/java/com/d23alex/vtbstat/db/repository/ArenaRepository.java
@@ -2,9 +2,8 @@ package com.d23alex.vtbstat.db.repository;
 
 
 import com.d23alex.vtbstat.entity.Arena;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ArenaRepository extends CrudRepository<Arena, Long> {
+
 }
