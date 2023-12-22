703902b7ba3aa16e8d9edfcb142f23e1ddd58ce3
Chesnokov Arkady <arkady-chesnokof@yandex.ru>
2023-12-22 16:54:35 +0300

    FIXME: the header does not stretch to its full length

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -10,6 +10,35 @@ import Footer from "./components/Footer.vue";
   <Footer/>
 </template>
 
-<style scoped>
+<style>
+  
+  body {
+    background-color: #ECE2D0;
+    margin: 0;
+    padding: 0;
+  }
 
+  header {
+    padding: 20px;
+    margin: 0;
+    background-color: #004777;
+    
+    height: 2.5vh;
+    width: 100%;
+    
+
+    /* position: sticky; */
+  }
+
+  header a {
+    float: left;
+    text-align: center;
+    padding: 12px;
+
+    color: #FFF;
+    text-decoration: none;
+    font-size: 18px;
+    line-height: 25px;
+    border-radius: 4px;
+  }
 </style>
--- ui/src/components/Header.vue
+++ ui/src/components/Header.vue
@@ -3,11 +3,20 @@
 </script>
 
 <template>
-<header>
-HEADER HERE
-</header>
+    <header>
+        <img class="itmo-logo" src="./icons/itmo-logo.png" alt="itmo logo"/>
+        <a href="">НОВОСТИ</a>
+        <a href="">КАЛЕНДАРЬ</a>
+        <a href="">КОМАНДЫ</a>
+        <a href="">СТАТИСТИКА</a>
+        <a href="">О НАС</a>
+    </header>
 </template>
 
-<style scoped>
-
+<style>
+   .itmo-logo {
+       width: 125px;
+       height: 75px;
+       float: left; 
+   } 
 </style>
