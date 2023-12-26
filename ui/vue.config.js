99685a168deff341be2e8f360fbd9c9a8f8d8cb7
D23Alex <aleksejdormorezov@gmail.com>
2023-12-26 13:12:02 +0300

    another attempt to fix gh pages

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/basketball-scorekeeper/' // note the trailing slash
-        : '/',
+    publicPath: '/basketball-scorekeeper/'
 }
