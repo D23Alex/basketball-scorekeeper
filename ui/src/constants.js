e352bce4f4fe45242c517c1aff606a41aaa4c10f
nokkov <arkady-chesnokof@yandex.ru>
2024-01-09 18:19:10 +0300

    change path

--- ui/src/constants.js
+++ ui/src/constants.js
@@ -3,14 +3,9 @@ export const TEAM_MAIN_COLOR_BY_ID = {
     2: "red",
     3: "orange"
 };
-<<<<<<< HEAD
-//FIXME
-export const API_HOST = "localhost";
-=======
 
 export const IMAGES_PATH = 'images'
 
 export const API_HOST = "158.160.137.233";
->>>>>>> b21af35eae7fa4d115bd0b4dd199fccff954c360
 export const API_PORT = "8080";
 export const API = "https://" + API_HOST + ":" + API_PORT + "/api";
